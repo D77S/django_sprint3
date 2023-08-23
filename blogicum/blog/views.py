19560b6ae6bf5568e95add12fef90894f2965d9a
Dmitry <d77s@inbox.ru>
2023-08-23 15:21:50 +0300

    Next

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,9 +1,8 @@
-# from typing import Any
-
-from http.client import HTTPResponse
-
+from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
+from blog.models import Post
 
 # posts: list[dict[str, Any]] = [
 #    {
@@ -49,17 +48,24 @@ from django.shortcuts import render
 # ]
 
 
-def index(request) -> HTTPResponse:
-    # temporary_posts = sorted(posts, key=lambda x: x['id'], reverse=True)
-    context = {'temporary_posts_transfer': posts}
+def index(request) -> HttpResponse:
+    posts = Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now()
+        ).order_by('-pub_date')[:5]
+    # Пока оставим тут лямбда-сортировку с прошлой версии проекта,
+    # вдруг когда потом пригодится.
+    # posts = sorted(posts, key=lambda x: x['id'], reverse=True)
+    context = {'post_list': posts, }
     return render(request, 'blog/index.html', context)
 
 
-def post_detail(request, id: int) -> HTTPResponse:
+def post_detail(request, id: int) -> HttpResponse:
     context = {'post': posts[id]}
     return render(request, 'blog/detail.html', context)
 
 
-def category_posts(request, category_slug: str) -> HTTPResponse:
+def category_posts(request, category_slug: str) -> HttpResponse:
     context = {'temporary_slug': category_slug}
     return render(request, 'blog/category.html', context)

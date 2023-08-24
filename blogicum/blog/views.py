c9ff8c8d8b8af5bada1ea74137a809f5e081681a
Dmitry <d77s@inbox.ru>
2023-08-24 15:09:11 +0300

    Next

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -27,11 +27,17 @@ def post_detail(request, id: int) -> HttpResponse:
 
 
 def category_posts(request, category_slug: str) -> HttpResponse:
-    posts1 = Post.objects.select_related('category').filter(
+    posts1 = Post.objects.select_related(
+        'category',
+        'location',
+        'author'
+        ).filter(
             category__slug=category_slug,
             category__is_published=True,
             pub_date__lte=datetime.now(tz=timezone.utc)
-            ).order_by('-pub_date')
+            ).order_by(
+                '-pub_date'
+                )
     cat1 = Category.objects.get(slug=category_slug)
     posts = get_list_or_404(posts1, is_published=True)
     context = {'post_list': posts, 'category': cat1}

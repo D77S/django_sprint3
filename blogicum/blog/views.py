62833d199c64b0e009dc6fe5bff4a8dd574ed08e
Дмитрий <d77s@inbox.ru>
2023-08-25 21:12:20 +0300

    Next

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,41 +1,71 @@
+from django.db.models import QuerySet
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.utils import timezone
-from datetime import datetime
 
 from blog.models import Post, Category
 
 
+def posts_selected() -> QuerySet:
+    """Функция, ничего не принимает,
+    возвращает модель (таблицу) Post
+    с заджойненными к ней моделями (таблицами)
+    Category, Location, User.
+    Джойнимся по именам полей модели Post.
+    Возвращаем вообще все поля, так как
+    из всех вызовов функции есть хотя бы один,
+    которому они нужны полностью все."""
+    return Post.objects.select_related('category',
+                                       'location',
+                                       'author'
+                                       )
+
+
 def index(request) -> HttpResponse:
-    posts = Post.objects.filter(is_published=True,
-                                category__is_published=True,
-                                #  pub_date__lte=datetime.now(tz=timezone.utc)
-                                pub_date__lte=timezone.now()
-                                ).order_by('-pub_date')[:5]
+    """Функция для отображения главной страницы,
+    принимает только стандартное заклинание request,
+    возвращает отрендеренную страницу (главную).
+    """
+    posts = posts_selected().filter(is_published=True,
+                                    category__is_published=True,
+                                    pub_date__lte=timezone.now()
+                                    ).order_by('-pub_date')[:5]
     context = {'post_list': posts, }
     return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, id: int) -> HttpResponse:
-    post1 = Post.objects.select_related(
-        'category').filter(pk=id, is_published=True,
-                           category__is_published=True)
-    post = get_object_or_404(
-        post1, pub_date__lte=timezone.now())
+    """Функция для отображения всех данных
+    по одному конкретному посту,
+    принимает стандартное заклинание request и
+    номер того поста, данные по которому надо отобразить,
+    возвращает отрендеренную страницу (пост детально).
+    """
+    posts = posts_selected().filter(pk=id, is_published=True,
+                                    category__is_published=True)
+    post = get_object_or_404(posts, pub_date__lte=timezone.now())
     context = {'post': post}
     return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug: str) -> HttpResponse:
-    temp_post_1 = Post.objects.select_related('category',
-                                              'location',
-                                              'author'
-                                              )
-    posts1 = temp_post_1.filter(category__slug=category_slug,
-                                category__is_published=True,
-                                pub_date__lte=datetime.now(tz=timezone.utc)
-                                ).order_by('-pub_date')
-    cat1 = Category.objects.get(slug=category_slug)
-    posts = get_list_or_404(posts1, is_published=True)
-    context = {'post_list': posts, 'category': cat1}
+    """Функция для отображения всех постов одной из категорий,
+    принимает стандартное заклинание request и
+    слаг той категории, все посты которой надо отобразить,
+    возвращает отрендеренную страницу (все посты заданной категории).
+    """
+    posts = posts_selected().filter(category__slug=category_slug,
+                                    category__is_published=True,
+                                    pub_date__lte=timezone.now()
+                                    ).order_by('-pub_date')
+    posts_or_404 = get_list_or_404(posts, is_published=True)
+    # Мы точно знаем, что в модели Category
+    # должнен быть объект (строка)
+    # со слагом, равным переданному в функцию,
+    # причем ровно один (уникальный).
+    # Далее нам понадобятся некоторые его поля,
+    # извлечем его весь для передачи в шаблон.
+    selected_category_or_404 = get_object_or_404(Category.objects.all(),
+                                                 slug=category_slug)
+    context = {'post_list': posts_or_404, 'category': selected_category_or_404}
     return render(request, 'blog/category.html', context)
